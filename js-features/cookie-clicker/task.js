af84cc7cfa3b8f2bc36bc4d5fd26f19e4d3460ef
Evteeva Ksenia <slepzova.ksenia@mail.ru>
2024-07-10 00:11:28 +0300

    cookie-clicker full done

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -2,10 +2,10 @@ const clicker = document.getElementById("clicker__counter");
 const cookie = document.getElementById("cookie");
 
 cookie.onclick = function () {
-    if (cookie.width == "200px") {
-        cookie.width == "250px";
+    if (cookie.width == 200) {
+        cookie.width = 250;
     } else {
-        cookie.width == "200px";
+        cookie.width = 200;
     }
     clicker.textContent = +(clicker.textContent) + 1;   
 };
