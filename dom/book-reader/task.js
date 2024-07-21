acaeedb91f032c7c34f2f0600fef0229dd399313
Evteeva Ksenia <slepzova.ksenia@mail.ru>
2024-07-22 01:08:20 +0300

    book-reader done

---
+++ dom/book-reader/task.js
@@ -0,0 +1,23 @@
+const book = document.querySelector('.book');
+const fontSize = document.querySelectorAll('.font-size');
+
+let changeSize = function() {
+    fontSize.forEach(element => {
+        element.addEventListener('click', (event) => {
+            event.preventDefault();
+            element.classList.add('font-size_active');
+            if(element.classList.contains('font-size_small')) {
+                book.classList.add('book_fs-small');
+                book.classList.remove('book_fs-big');
+            } else if(element.classList.contains('font-size_big')) {
+                book.classList.add('book_fs-big');
+                book.classList.remove('book_fs-small');
+            } else {
+                book.classList.remove('book_fs-big');
+                book.classList.remove('book_fs-small');
+            }
+        })
+    })
+}
+
+changeSize();
