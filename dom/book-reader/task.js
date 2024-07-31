c73a5513f7d978b37517343143f13bf2830a7df0
Evteeva Ksenia <slepzova.ksenia@mail.ru>
2024-07-31 14:46:42 +0300

    book-reader full done

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -5,6 +5,7 @@ let changeSize = function() {
     fontSize.forEach(element => {
         element.addEventListener('click', (event) => {
             event.preventDefault();
+            fontSize.forEach((element) => element.classList.remove("font-size_active"));
             element.classList.add('font-size_active');
             if(element.classList.contains('font-size_small')) {
                 book.classList.add('book_fs-small');
