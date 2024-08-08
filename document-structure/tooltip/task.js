ef2213bcb68dda64f6b3aea5085f530df4b34391
Evteeva Ksenia <slepzova.ksenia@mail.ru>
2024-08-08 14:16:49 +0300

    tooltip full done

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,6 +1,7 @@
 const tooltipsLinks = Array.from(document.querySelectorAll('.has-tooltip'));
 const tooltipCount = 1;
 
+
 tooltipsLinks.forEach(tooltipLink => {
   tooltipLink.addEventListener('click', (event) => {
     event.preventDefault();
@@ -31,6 +32,8 @@ tooltipsLinks.forEach(tooltipLink => {
     tooltip.style.top = event.target.getBoundingClientRect().top + 20 + 'px';
     tooltip.style.left = event.target.getBoundingClientRect().left + 'px';
 
-    tooltipLink.appendChild(tooltip);
+    document.body.append(tooltip);
+    
   });
-});
+}); 
+
